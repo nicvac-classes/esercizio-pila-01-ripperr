524d26d834e777f95deec77efaccd9543eff9c8a
FNC-ninoMorisco <giov.morisco@gmail.com>
2026-04-20 19:27:28 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,4 +1,83 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+public class Nodo<T>{
+    T dato;
+    Nodo<T> next;
+
+    public Nodo(T dato){
+        this.dato = dato;
+        this.next = null;
+    }
+}
+
+java.util.NoSuchElementException;
+
+public class Pila<T>{
+    private Nodo<T> top;
+
+    public Pila(){
+        this.top = null;
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public void push(T dato){
+        Nodo<T> nuovoNodo = new Nodo<>(dato);
+
+        nuovoNodo.next = top;
+        top = nuovoNodo;
+    }
+
+    public T pop(){
+        if(top == null){
+            throw new NoSuchElementException("Pila vuota!");
+        }
+
+        T dato = top.dato;
+        top = top.next;
+
+        return dato;
+    }
+
+    public T peek(){
+        if(top == null){
+            throw new NoSuchElementException("Pila vuota!");
+        }
+
+        return top.dato;
+    }
+
+    public int size(){
+        Nodo<T> attuale = top;
+
+        int count = 0;
+        while(attuale != null){
+            attuale = attuale.next;
+            ++count;
+        }
+
+        return count;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+
+        Nodo<T> attuale = top;
+
+        str.append("top");
+        while (attuale != null) {
+            str.append("--->");
+            str.append(attuale.dato);
+            
+            attuale = attuale.next;
+        }
+
+        str.append("--->NULL");
+
+        return str.toString();
+    }
+}
 
 //Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
@@ -6,21 +85,30 @@ import java.util.Scanner;
 // Classe principale, con metodo main
 class Esercizio {
     // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        Pila<Character> pila = new Pila<>();
+        String parola;
+        do{
+            
+            System.out.print("Inserire una parola: ");
+            parola = in.nextLine();
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+            if(!parola.equalsIgnoreCase("X")){
+                for(int i = 0; i < parola.length(); ++i){
+                    pila.push(parola.charAt(i));
+                }
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+                String palindrom = "";
+                while(!pila.isEmpty()){
+                    palindrom += pila.pop();
+                }
+
+                if(parola.equalsIgnoreCase(palindrom)){
+                    System.out.println("La parola: " + parola + " è palindorma!!");
+                }
+            }
+        }while(!parola.equalsIgnoreCase("x"));
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
